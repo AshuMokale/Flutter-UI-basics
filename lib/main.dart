ec1313b531ec6b2121c8412ecce8574d9a897ec2
Ashish Mokale <ashishmokale2002@gmail.com>
2024-02-09 19:30:36 +0530

    Material Components program.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
-      ),
+    const MaterialApp(
+      title: 'UI Basics',
+      home: MyHomePage(),
     ),
   );
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation Menu',
+          onPressed: null,
+        ),
+        title: const Text('My App Bar'),
+        actions: const [
+          IconButton(
+          icon: Icon(Icons.search),
+          tooltip: 'Search',
+          onPressed: null,
+          ),
+        ],
+      ),
+      
+      // body
+      body: const Center(
+        child: Text('Hello World!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
