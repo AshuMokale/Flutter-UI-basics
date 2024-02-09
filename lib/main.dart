31885b7e02f4e770cd673892908a4e10afdb76d8
Ashish Mokale <ashishmokale2002@gmail.com>
2024-02-09 19:48:23 +0530

    Gesture detection program.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-      'Hello World!',
-      textDirection: TextDirection.ltr,
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
       ),
     ),
   );
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was pressed');
+      },
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Touch Me')
+        ),
+      ),
+    );
+  }
 }
