95ce814bd1138bd57ccf8f10647dc97d684c2bbc
Ashish Mokale <ashishmokale2002@gmail.com>
2024-02-09 18:56:56 +0530

    Basic Widget program.

--- lib/main.dart
+++ lib/main.dart
@@ -2,41 +2,69 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      theme: ThemeData(
-        // brightness: Brightness.dark,
-        primaryColor: Colors.blueGrey
-      ),
-      title: 'UI Test App',
-      home: Scaffold(
-        body: Center(
-          child: MyButton(),
-        ),
+    const MaterialApp(
+      title: 'UI Basics',
+      home: SafeArea(
+        child: MyScaffold(),
       ),
     ),
   );
 }
 
-class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+
+  final Widget title;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print('MyButton was tapped.');
-      },
-      child: Container(
-        height: 50,
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.symmetric(horizontal: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.green,
-        ),
-        child: const Center(
-          child: Text('Engage'),
-        ),
+    return Container(
+      height: 56,
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(color: Colors.blue),
+
+      // Row
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation Menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'My App Bar',
+              style: Theme.of(context).primaryTextTheme.titleLarge,
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+        ],
       ),
     );
   }
